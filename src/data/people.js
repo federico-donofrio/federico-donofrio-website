7b36edf45547cab349742eb97c3ee718b2d812ca
Federico D'Onofrio <federicodonofrio@proton.me>
2026-08-12 19:01:56 +0200

    Initial website

---
+++ src/data/people.js
@@ -0,0 +1,67 @@
+export const currentPeople = {
+  phdStudents: [
+    {
+      name: "Myriam Lichani",
+      affiliation: "University of Vienna",
+      dissertation:
+        "The life-cycle of farm accountancy data in Interwar Austria",
+      supervisors: []
+    },
+    {
+      name: "Mariia Romanova",
+      affiliation: "University of Vienna",
+      dissertation:
+        "Redefining Profit: Multilevel Perspectives on the Reform of Accounting in Russia's Economic Transition",
+      supervisors: ["Federico D’Onofrio", "Wolfgang Mueller"]
+    },
+    {
+      name: "Filippo Soramel",
+      affiliation: "University of Vienna; Université Paris-1",
+      dissertation:
+        "Property, Patriotism, and the Environment: Agricultural Societies and State-building in the Eastern Alps (1762-1820)",
+      supervisors: [
+        "Federico D’Onofrio",
+        "Christine Lebeau",
+        "Peter Mata"
+      ]
+    }
+  ],
+
+  researchAssistants: [
+    {
+      name: "Simone di Giovanni"
+    },
+    {
+      name: "Rosa Scamardella"
+    }
+  ]
+};
+
+export const formerPeople = {
+  postdocs: [
+    {
+      name: "Marina Chuchko Kisling",
+      outputs: [
+        {
+          title: "European Agriculture and the Great Depression: New Evidence from Farm Accountancy Data",
+          status: "Under revision"
+        },
+        {
+          title:
+            "The evolution of representativeness criteria in agricultural economics: the case of farm accountancy data",
+          authors: "D’Onofrio, F. and Chuchko, M.",
+          status: "In preparation"
+        }
+      ]
+    }
+  ],
+
+  researchAssistants: [
+    {
+      name: "Robin Jenny"
+    },
+    {
+      name: "Leonard Lienerbruenn"
+    }
+  ]
+};
---
+++ src/data/publications.js
@@ -0,0 +1,197 @@
+export const publications = {
+  books: [
+    {
+      year: 2016,
+      authors: "D’Onofrio, F.",
+      title: "Observing Agriculture in Early Twentieth-Century Italy: Agricultural Economists and Statistics",
+      details: "London and New York: Routledge.",
+      link: ""
+    },
+    {
+      year: 2025,
+      authors: "Joly, N. and D’Onofrio, F. (eds)",
+      title: "The Invention of the Accounting Farmer",
+      details: "Woodbridge: Boydell & Brewer.",
+      link: ""
+    }
+  ],
+
+  articles: [
+    {
+      year: 2025,
+      authors: "D’Onofrio, F. and Mignemi, N.",
+      title: "The making of perfect markets: Agriculture and international statistics in the early 20th century",
+      details: "Journal of World History, 36(3), pp. 409–439.",
+      link: "https://doi.org/10.1353/jwh.2025.a974184"
+    },
+    {
+      year: 2023,
+      authors: "D’Onofrio, F. and Mignemi, N.",
+      title: "The International Institute of Agriculture and the information infrastructure of world trade (1905–1947)",
+      details: "Histoire et Mesure, 38(1), pp. 13–38.",
+      link: "https://doi.org/10.4000/histoiremesure.19003"
+    },
+    {
+      year: 2023,
+      authors: "D’Onofrio, F. and Mignemi, N.",
+      title: "Global figures: Tools for observing and governing agricultural markets (1880s–1940s), Introduction",
+      details: "Histoire et Mesure, 38(1), pp. 5–12.",
+      link: ""
+    },
+    {
+      year: 2022,
+      authors: "D’Onofrio, F. and Tyler, H.",
+      title: "Wolf Ladejinsky: Field Trips for a Democratic Countryside",
+      details: "History of Political Economy, 54(3), pp. 459–481.",
+      link: "https://doi.org/10.1215/00182702-9779614"
+    },
+    {
+      year: 2021,
+      authors: "D’Onofrio, F.",
+      title: "Pubblica felicità, storia e Provvidenza: l’Economia Civile di Genovesi nel contesto dell’Assolutismo Illuminato",
+      details: "Il pensiero economico italiano, 29(1), pp. 27–45.",
+      link: ""
+    },
+    {
+      year: 2020,
+      authors: "Allisson, F., D’Onofrio, F., Raskov, D. and Shirokorad, L.",
+      title: "Marxism before Marxism: Nikolaj Sieber and the birth of Russian social-democracy",
+      details: "European Journal of the History of Economic Thought, 27(4), pp. 298–323.",
+      link: ""
+    },
+    {
+      year: 2020,
+      authors: "D’Onofrio, F. and Serra, G.",
+      title: "Hidden agencies: Economists and authoritarian regimes in the 20th century",
+      details: "Research in the History of Economic Thought and Methodology, 38(B), pp. 3–16.",
+      link: ""
+    },
+    {
+      year: 2019,
+      authors: "D’Onofrio, F. and Serra, G.",
+      title: "Arthur Lewis and the Classical Foundations of ‘Development’: Economic History and Institutional Change",
+      details: "Research in the History of Economic Thought and Methodology, 37A, pp. 157–164.",
+      link: ""
+    },
+    {
+      year: 2018,
+      authors: "D’Onofrio, F.",
+      title: "La nazione meglio polita: buon governo e costituzione economica della Cina alla scuola di Genovesi",
+      details: "Società e Storia, 161, pp. 471–497.",
+      link: ""
+    },
+    {
+      year: 2017,
+      authors: "D’Onofrio, F.",
+      title: "Agricultural numbers: the statistics of the International Institute of Agriculture in the Interwar period",
+      details: "Agricultural History Review, 65(2), pp. 277–296.",
+      link: ""
+    },
+    {
+      year: 2017,
+      authors: "D’Onofrio, F.",
+      title: "Micro-foundations of Italian agrarianism: Italian agricultural economists and fascism",
+      details: "Agricultural History, 92(3), pp. 369–396.",
+      link: ""
+    },
+    {
+      year: 2015,
+      authors: "D’Onofrio, F.",
+      title: "On the concept of ‘Felicitas Publica’ in Eighteenth Century Political Economy",
+      details: "Journal of the History of Economic Thought, 37(3), pp. 449–471.",
+      link: ""
+    },
+    {
+      year: 2012,
+      authors: "D’Onofrio, F.",
+      title: "Making variety simple: agricultural economists in Southern Italy, 1907–1909",
+      details: "History of Political Economy, 44, pp. 93–113.",
+      link: ""
+    }
+  ],
+
+  chapters: [
+    {
+      year: 2026,
+      authors: "D’Onofrio, F.",
+      title: "From the International Institute of Agriculture to the Food and Agriculture Organisation",
+      details: "In Cabo, M., Fernández Prieto, L. and Pan-Montojo, J. (eds), From rural modernities to agricultural modernisation. Technologies and discourses on progress in the 20th century, Woodbridge: Boydell & Brewer, pp. 101–118.",
+      link: ""
+    },
+    {
+      year: 2025,
+      authors: "D’Onofrio, F.",
+      title: "The intentional spread of farm accountancy offices in Europe 1870–1950",
+      details: "In Joly, N. and D’Onofrio, F. (eds), The Invention of the Accounting Farmer, Woodbridge: Boydell & Brewer.",
+      link: ""
+    },
+    {
+      year: 2025,
+      authors: "Joly, N. and D’Onofrio, F.",
+      title: "Introduction",
+      details: "In Joly, N. and D’Onofrio, F. (eds), The Invention of the Accounting Farmer, Woodbridge: Boydell & Brewer.",
+      link: ""
+    },
+    {
+      year: 2023,
+      authors: "D’Onofrio, F.",
+      title: "Il neoliberalismo al tramonto della globalizzazione: D’Onofrio legge Slobodian",
+      details: "Storica, 86, pp. 187–200.",
+      link: ""
+    },
+    {
+      year: 2019,
+      authors: "D’Onofrio, F.",
+      title: "Numeri falsi e numeri errati: le basi informative della politica granaria a Napoli nella seconda metà del secolo XVIII",
+      details: "In Clemente, A. and Russo, S. (eds), La polizia de' grani: Mercati, regole e crisi di sussistenza nelle economie di Antico Regime, Roma: Gangemi, pp. 161–180.",
+      link: ""
+    },
+    {
+      year: 2013,
+      authors: "D’Onofrio, F.",
+      title: "Les statistiques agricoles de l’Italie libérale et les porteurs d’intérêts (1907–1929)",
+      details: "In Fraboulet, D., Druelle-Korn, C. and Vernus, P. (eds), Les organisations patronales et la sphère publique. Europe XIXe et XXe siècles, Rennes: PUR, pp. 143–152.",
+      link: ""
+    },
+    {
+      year: 2007,
+      authors: "D’Onofrio, F.",
+      title: "Giordano Bruno e la favola di Deucalione",
+      details: "In Catanorchi, O. and Pirillo, D. (eds), Sogni, favole, storie. Seminario su Giordano Bruno, Pisa: Edizioni della Scuola Normale.",
+      link: ""
+    }
+  ]
+};
+
+
+export const pipeline = [
+  {
+    title: "The experience of value and the value of experience: land valuation in the 19th and 20th century",
+    status: "Submitted to Science in Context",
+    link: ""
+  },
+  {
+    title: "The evolution of representativeness criteria in agricultural economics: the case of farm accountancy data",
+    authors: "D’Onofrio, F. and Chuchko, M.",
+    status: "",
+    link: ""
+  },
+  {
+    title: "Coup d’Oeil: An early-modern concept to understand decision-making",
+    authors: "D’Onofrio, F.",
+    status: "",
+    link: ""
+  },
+  {
+    title: "The modern fact and the antiquarian: the philological origins of political economy",
+    authors: "D’Onofrio, F.",
+    status: "",
+    link: ""
+  },
+  {
+    title: "French Grain Trade and the International Market (1720 to 1820)",
+    authors: "Charles, L., Daudin, G. and D’Onofrio, F.",
+    status: "",
+    link: ""
+  }
+];
